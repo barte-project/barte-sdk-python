df4db79339dd3819d0f25f6f49b76f2b257e9a42
wilsonbarte <wilson@gmail.com>
2024-05-30 08:00:29 -0300

    Add base module

--- bartesdk/bartesdk/subseller.py
+++ bartesdk/bartesdk/subseller.py
@@ -1,9 +1,9 @@
-import requests
+from .base import BaseAPI
 
-class subsellerAPI:
-    def __init__(self, api_key, env="prd", api_version="v2"):
-        self.api_key = api_key
-        self.base_url = self._get_base_url(env, api_version)
+class subsellerAPI(BaseAPI):
+    def __init__(self, api_key, env="prd", api_version="v1"):
+        base_url = self._get_base_url(env, api_version)
+        super().__init__(api_key, base_url)
 
     def _get_base_url(self, env, api_version):
         if env == "prd":
@@ -12,39 +12,3 @@ class subsellerAPI:
             return f'https://sandbox-api.barte.com/{api_version}/seller/subseller'
         else:
             raise ValueError("Invalid environment specified")
-
-    def getByID(self, id):
-        headers = {
-            'X-Token-Api': self.api_key,
-            'accept': '*/*'
-        }
-        url = f"{self.base_url}/{id}"
-        response = requests.get(url, headers=headers)
-        return response.status_code, response.json() if response.ok else response.text
-
-    def update(self, uuid, **kwargs):
-        headers = {
-            'X-Token-Api': self.api_key,
-            'Content-Type': 'application/json',
-            'accept': '*/*'
-        }
-        url = f"{self.base_url}/{uuid}"
-        payload = kwargs
-        response = requests.put(url, headers=headers, json=payload)
-        try:
-            response_data = response.json() if response.ok else response.text
-        except ValueError:
-            response_data = response.text
-        return response.status_code, response_data
-
-    def create(self, **kwargs):
-        headers = {
-            'X-Token-Api': self.api_key,
-            'Content-Type': 'application/json',
-            'accept': 'application/json'
-        }
-        payload = kwargs
-        response = requests.post(self.base_url, headers=headers, json=payload)
-        return response.status_code, response.json() if response.ok else response.text
-
-
