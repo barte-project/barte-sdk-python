4e374858b7acd62f43b78ecd0479b435401c9354
wilsonbarte <wilson@gmail.com>
2024-05-31 07:10:29 -0300

    Adjust path API subseller

--- bartesdk/bartesdk/subseller.py
+++ bartesdk/bartesdk/subseller.py
@@ -2,5 +2,5 @@ from .base import BaseAPI
 
 class subsellerAPI(BaseAPI):
     def __init__(self, api_key, env="prd", api_version="v1"):
-        path = "/seller/subseller"
+        path = "seller/subseller"
         super().__init__(api_key, env, api_version, path)
