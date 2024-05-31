ce19ddf7f2a7f279b9e0aa51cc065d236bdc9dce
wilsonbarte <wilson@gmail.com>
2024-05-31 07:38:48 -0300

    Update base adding non-generic function

--- bartesdk/bartesdk/base.py
+++ bartesdk/bartesdk/base.py
@@ -5,6 +5,7 @@ class BaseAPI:
         self.api_key = api_key
         self.base_url = self._get_base_url(env, api_version, path)
 
+
     def _get_base_url(self, env, api_version, path):
         if env == "prd":
             return f'https://api.barte.com/{api_version}/{path}'
@@ -12,6 +13,10 @@ class BaseAPI:
             return f'https://sandbox-api.barte.com/{api_version}/{path}'
         else:
             raise ValueError("Invalid environment specified")
+
+
+    # Generic Funcion      
+
 
     def create(self, **kwargs):
         headers = {
@@ -23,6 +28,7 @@ class BaseAPI:
         response = requests.post(self.base_url, headers=headers, json=payload)
         return response.status_code, response.json() if response.ok else response.text
 
+
     def get(self, **params):
         headers = {
             'X-Token-Api': self.api_key,
@@ -32,6 +38,7 @@ class BaseAPI:
         response = requests.get(self.base_url, headers=headers, params=params)
         return response.json()
 
+
     def getByUuid(self, uuid):
         headers = {
             'X-Token-Api': self.api_key
@@ -39,6 +46,7 @@ class BaseAPI:
         url = f"{self.base_url}/{uuid}"
         response = requests.get(url, headers=headers)
         return response.json()
+
 
     def update(self, uuid, **kwargs):
         headers = {
@@ -55,6 +63,7 @@ class BaseAPI:
             response_data = response.text
         return response.status_code, response_data
 
+
     def cancel(self, uuid):
         headers = {
             'X-Token-Api': self.api_key,
@@ -63,3 +72,73 @@ class BaseAPI:
         url = f"{self.base_url}/{uuid}"
         response = requests.delete(url, headers=headers)
         return response.status_code, response.json() if response.ok else response.text
+
+
+    # Non Generic Function
+    
+    def refund(self, uuid, as_fraud=True):
+        headers = {
+            'X-Token-Api': self.api_key,
+            'Content-Type': 'application/json',
+            'accept': '*/*'
+        }
+        url = f"{self.base_url}/{uuid}/refund"
+        payload = {
+            'asFraud': as_fraud
+        }
+        response = requests.patch(url, headers=headers, json=payload)
+        return response.status_code, response.ok
+
+
+    def calculateInstallments(self, **kwargs):
+        headers = {
+            'X-Token-Api': self.api_key,
+            'accept': '*/*'
+        }
+        response = requests.get(self.base_url, headers=headers, params=kwargs)
+        try:
+            response_data = response.json() if response.ok else response.text
+        except ValueError:
+            response_data = response.text
+        return response.status_code, response_data
+
+
+    def maxInstallments(self):
+        headers = {
+            'X-Token-Api': self.api_key,
+            'accept': '*/*',
+            'Content-Type': 'application/json'
+        }
+        url = f"{self.base_url}/max-installments"
+        response = requests.get(url, headers=headers)
+        return response.status_code, response.json() if response.ok else response.text
+
+
+    def installmentsPayment(self, **kwargs):
+        headers = {
+            'X-Token-Api': self.api_key,
+            'accept': '*/*',
+            'Content-Type': 'application/json'
+        }
+        url = f"{self.base_url}/installments-payment"
+        response = requests.get(url, headers=headers, params=kwargs)
+        try:
+            response_data = response.json() if response.ok else response.text
+        except ValueError:
+            response_data = response.text
+        return response.status_code, response_data
+
+
+    def updateBasicValue(self, subscription_id, **kwargs):
+        headers = {
+            'X-Token-Api': self.api_key,
+            'Content-Type': 'application/json',
+            'accept': '*/*'
+        }
+        url = f"{self.base_url}/{subscription_id}/basic-value"
+        response = requests.patch(url, headers=headers, json=kwargs)
+        try:
+            response_data = response.json() if response.ok else response.text
+        except ValueError:
+            response_data = response.text
+        return response.status_code, response_data
